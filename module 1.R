803d59a0862c78eac498a0fb67c36490d679dda8
courtneycheung <112443893+courtneycheung@users.noreply.github.com>
2022-09-11 17:42:48 -0400

    Add files via upload

--- module 1.R
+++ module 1.R
@@ -1,52 +1,39 @@
 library(ggplot2)
 library(tidyverse)
-
-dat <- read.csv("~/experimental_methods_in_organismal_bio/Data/scales.csv")
-
+library(ggplot2)
+setwd("~/Downloads")
+#a dat variable containing the scales dataset.
+dat <- read.csv("scales.csv") 
+#line of code reporting the class of each column in the dataset
+sapply(dat,class)
+#line of code reporting the dimensions of the dataset
 dim(dat)
-
 head(dat)
-
-sapply(dat, class)
-
-
+class(dat$N)
+class(dat[,1])
 dat$species <- as.factor(dat$species)
 species <- levels(dat$species)
 species
-
 length(species)
-
 dat$species==species[1]
-
-dat$species[dat$species==1]
-
-A.rup<-length(dat$species[dat$species==species[1]])
-L.gib<-length(dat$species[dat$species==species[2]])
-L.mac<-length(dat$species[dat$species==species[3]])
-M.sal<-length(dat$species[dat$species==species[4]])
-M.sax<-length(dat$species[dat$species==species[5]])
-P.fla<-length(dat$species[dat$species==species[6]])
-
-species.obs <- data.frame(sp=species,n=c(A.rup, L.gib, L.mac,M.sal,M.sax,P.fla))
-species.obs
-
+dat %>% 
+  count(species,quadrant) %>%
+  print()
+#code that produces a summary of the number of scales punctured for each species at each location
+dat %>% 
+  count(species,specimen,name = "n.specimens") %>%
+  print()
+#code that produces a summary of the number of specimens sampled for each species.
 dat %>%
   group_by(species) %>%
-  summarise(n=n())
-
-dat %>% 
-  count(species,specimen) %>%
-  print() %>%
-  count(species,name = "n.specimens")
-
+  summarise(n = n())
 for(i in species){
   p <- dat %>%
     filter(species==i)%>%
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
-}
-
-pdf("species.quadrant.Joe.Milosh.pdf")
+#code creating a pdf of plot figures
+}pdf("species.quadrant.pdf")
 for(i in species){
   p <- dat %>%
     filter(species==i)%>%
@@ -54,6 +41,3 @@ for(i in species){
   print(p)
 }
 dev.off()
-
-list.files(pattern=".pdf")
-
