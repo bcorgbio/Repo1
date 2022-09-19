89b1a24ba1182b94edfa507b2c965f1c4aa27f61
miloshj1 <83241739+miloshj1@users.noreply.github.com>
2022-09-18 20:55:16 -0400

    module 2 final

--- Joseph.Milosh_module 2.R
+++ Joseph.Milosh_module 2.R
@@ -1,11 +1,23 @@
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
+library(features)
 
 pseed <- read_csv("pseed.fin.amps.csv")
 pseed.bl <- read_csv("pseed.lengths.csv")
 speeds <- read_csv("pseed.calibration.csv")
 
+#1. recreating pseed.wide
+pseed.wide <- pseed%>%
+  left_join(speeds,by=c("speed"="vol"))%>%
+  left_join(pseed.bl,by="fish")%>%
+  mutate(bl.s=cm.s/bl)%>%
+  select(-amp)%>%
+  pivot_wider(names_from = fin,values_from = amp.bl) %>%
+  mutate(amp.sum=L+R)%>%
+  print()
+
+#2. Code giving the mean maximum of all of the amp.sum
 find.peaks <- function(x,y,mult=100){
   f <- fget(features(x = x,y=y*mult))[2:3]%>% 
     as_tibble() %>% 
@@ -13,37 +25,45 @@ find.peaks <- function(x,y,mult=100){
     mutate(peaks=round(crit.pts,0))
 return(f$peaks) 
 }
-#code above gives the peaks and critical points for each fish in each experiment; this gives us the pixel value for where the peak occurs. If we were to join this with the other data set then we would get ride of all the non-peak values
 
-pseed.sum.max <- pseed2 %>% 
-  group_by(date,fish,bl.s) %>% 
-  mutate(peak=frame %in% find.peaks(frame,amp.sum)) %>% 
-  filter(peak==T) %>% 
-  group_by(fish,bl.s) %>% 
-  summarize(amp.sum.mean=mean(amp.sum))
-pseed.sum.max
+pseed.sum.max <- pseed.wide%>%
+  group_by(date,fish,bl.s)%>%
+  mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
+  filter(peak==T)%>%
+  group_by(date,fish,bl.s)%>%
+  summarize(amp.sum.mean=mean(amp.sum)) %>% 
+  print()
 
-#function for se
+#3.function for se
 se <- function(x){
   sd(x) / sqrt(length(x))}
 
 #code creating a column for se and visualizing the change
-pseed.sum.max <- pseed.sum.max %>% 
-  group_by(fish) %>% 
-  mutate(amp.sum.se=se(amp.sum.mean)) %>% 
-  print()
-
-#code plotting amp.sum.mean vs specific swim speed
+pseed.sum.max <- pseed.wide%>%
+    group_by(date,fish,bl.s)%>%
+    mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
+    filter(peak==T) %>% 
+    group_by(date,fish,bl.s) %>% 
+    summarize(amp.sum.se=SE(amp.sum)) %>% 
+    left_join(pseed.sum.max,by=c("date"="date","fish"="fish","bl.s"="bl.s"))%>%
+    print()  
+  
+#4. code plotting amp.sum.mean vs specific swim speed with error bars
 pseed.sum.max %>%
   ggplot(aes(x=bl.s,y=amp.sum.mean,col=fish))+geom_point()+geom_errorbar(aes(ymin=amp.sum.mean-amp.sum.se,ymax=amp.sum.mean+amp.sum.se))
 
-#reading new file as a tibble and merging it with pseed.sum.max
+#5. reading new file as a tibble and merging it with pseed.sum.max
 pseed.met.rate <-read_csv("pseed.met.rate.csv")
 as_tibble(pseed.met.rate)
+
+pseed.met.rate$bl.s <- as.numeric(as.character(pseed.met.rate$bl.s))
+pseed.sum.max$bl.s <- as.numeric(as.character(pseed.sum.max$bl.s))
+
 pseed.sum.max <- pseed.sum.max %>% 
-  left_join(pseed.met.rate,by=c("fish"="fish")) %>% 
+  left_join(pseed.met.rate,by=c("date"="date","fish"="fish","bl.s"="bl.s")) %>% 
   print()
 
-#metabolic power output of each fish vs mean maximum of amp.sum
+#6. plot of metabolic power output of each fish vs mean maximum of amp.sum
 pseed.sum.max %>%
   ggplot(aes(x=met.rate,y=amp.sum.mean,col=fish))+geom_point()
+
