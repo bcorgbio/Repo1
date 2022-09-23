5a288ffe1748ea56d5fb5a4799bcc3c06e194c53
miloshj1 <83241739+miloshj1@users.noreply.github.com>
2022-09-23 19:05:57 -0400

    notes

--- module3 Joe.R
+++ module3 Joe.R
@@ -26,7 +26,7 @@ anole.log <- anole2%>%
 anole2%>%
   ggplot(aes(SVL,HTotal))+geom_point()+geom_smooth(method="lm")
 
-anole.lm <- lm(HTotal~SVL,anole2)
+anole.lm <- lm(HTotal+SVL~PH,anole2)
 
 coef(anole.lm)
 
@@ -76,3 +76,100 @@ anole.log <- anole.log %>%
 
 anole.log%>%
   ggplot(aes(Ecomorph2,res))+geom_point()
+
+p.eco <- anole.log%>%
+  ggplot(aes(x=Ecomorph2,y=res)) +geom_boxplot()
+print(p.eco)
+
+p.eco+ geom_boxplot() +stat_summary(fun=mean, geom="point", size=3)
+
+anole.tree <- read.tree("anole.tre")
+plot(anole.tree,cex=0.4)
+
+#PGLS under BM, no ecomorph
+pgls.BM1 <- gls(HTotal ~SVL, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+#PGLS under BM, w ecomorph
+pgls.BM2 <- gls(HTotal ~SVL * Ecomorph2, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+
+#PGLS under OU, no ecomorph
+pgls.OU1 <- gls(HTotal ~SVL, correlation = corMartins(0,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+#PGLS under OU, w, ecomorph
+pgls.OU2 <- gls(HTotal ~SVL * Ecomorph2, correlation = corMartins(0,phy = anole.tree,form=~Species),data = anole.log, method = "ML")
+
+anole.phylo.aic <- AICc(pgls.BM1,pgls.BM2,pgls.OU1,pgls.OU2)
+aicw(anole.phylo.aic$AICc)
+
+anova(pgls.BM2)
+
+anole.log <- anole.log%>%
+  mutate(phylo.res=residuals(pgls.BM2))
+
+p.eco.phylo <- anole.log%>%
+  ggplot(aes(x=Ecomorph2,y=phylo.res)) +geom_boxplot() +stat_summary(fun=mean, geom="point", size=3)
+
+print(p.eco.phylo)
+
+anole.log%>%
+  dplyr::select(Ecomorph2,res,phylo.res)%>%
+  pivot_longer(cols=c("res","phylo.res"))%>%
+  print%>%
+  ggplot(aes(x=Ecomorph2,y=value)) +geom_boxplot() +stat_summary(fun=mean, geom="point", size=3)+facet_grid(name~.,scales = "free_y")+ylab("residual")
+
+#1.
+anole.log <- anole %>%  
+  left_join(anole.eco) %>% 
+  filter(!Ecomorph %in% c("U","CH")) %>% 
+  na.omit() %>% 
+  mutate_at(c("SVL","HTotal","PH","ArbPD"), log) %>% 
+  print()
+
+#2
+anolePH.lm <- lm(HTotal ~ SVL + PH,anole.log)
+anolePD.lm <- lm(HTotal ~ SVL + ArbPD,anole.log)
+
+#3
+anole.log.res <- anole.log %>% 
+  mutate(lmPHres=residuals(anolePH.lm)) %>% 
+  mutate(lmPDres=residuals(anolePD.lm))
+
+print(anole.log.res)
+
+anolePH.log.res.plot <- anole.log.res %>% 
+  ggplot(aes(x=PH,y=lmPHres))+geom_point()
+anolePH.log.res.plot
+
+anolePD.log.res.plot <- anole.log.res %>% 
+  ggplot(aes(x=PD,y=lmPDres))+geom_point()
+anolePH.log.res.plot
+
+#4 Under a BM model of trait evolution and using the tree provided,
+#construct phylogenetic least squares models of the hindlimb-SVL
+#relationships that include the unique combinations of these two covariates,i.e,
+#* A PGLS model with the hindlimb-SVL relationship + perch height
+#* A PGLS model with the hindlimb-SVL relationship + perch diameter
+#* A PGSL model with the hindlimb-SVL relationship + perch height + perch diameter
+
+#PGLS under BM, + perch height
+pgls.BM1 <- gls(HTotal ~SVL+PH, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log.res, method = "ML")
+
+#PGLS under BM, + perch diameter
+pgls.BM2 <- gls(HTotal ~SVL+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log.res, method = "ML")
+
+#PGLS under BM, + perch height + perch diameter
+pgls.BM3 <- gls(HTotal ~SVL+PH+ArbPD, correlation = corBrownian(1,phy = anole.tree,form=~Species),data = anole.log.res, method = "ML")
+
+#5.
+anole.aic <- AICc(pgls.BM1,pgls.BM2,pgls.BM3)
+aicw(anole.aic$AICc)
+#        fit     delta           w
+#1 -64.77956 10.746149 0.003247185
+#2 -73.81081  1.714901 0.296905077
+#3 -75.52571  0.000000 0.699847738
+
+anova(pgls.BM3)
+#both covariates are significant predictors of hind-limb-SVL relationship
+#both p-values are <0.05)
+
